6338d2127c47f5f7e32da623fbd4283416ab06f9
xiongxiangquan <xiongxiangquan@landicorp.com>
2017-05-23 09:37:19 +0800

    历史消息优化

--- page/subscription/pages/history/index.js
+++ page/subscription/pages/history/index.js
@@ -18,55 +18,46 @@ Page({
     var list = github.getSubs();
     this.totle = list.length;
     this.count = 0;
+    var that = this;
 
     if (0 === this.totle) {
       that.setData({
         issuesList: []
       });
     } else {
-      list.map(this.get, this);
+      Promise.all(list.map(that.get, that))
+        .then(function () {
+          console.log(that.data.issuesList[0]);
+
+          that.data.issuesList.sort(function(a, b){
+
+          })
+          that.setData({
+            issuesList: that.data.issuesList.slice(0, 100)
+          });
+        }).catch(function (err) {
+          console.log('catch exception' + err)
+        })
     }
   },
   get: function (item) {
     var that = this;
-    github.getUserInfo(item).then(function (userinfo) {
+    return github.getUserInfo(item).then(function (userinfo) {
       that.count = that.count + 1;
       userinfo = Object.assign({}, item, userinfo);
-      return new Promise(function (resolve, reject) {
-        github.getIssues(userinfo).then(function (issues) {
-          issues.map(function (item) {
-            item.created_at = item.created_at.split('T')[0];
-            item.owner = userinfo.owner;
-            item.repo = userinfo.repo;
-          });
-          return new Promise(function (resolve, reject) {
-            resolve(issues);
-            return;
-          });
-        }).then(function (issues) {
-          var tempList = that.data.issuesList;
-          that.data.issuesList = tempList.concat(issues);
+      return github.getIssues(userinfo).then(function (issues) {
+        issues.map(function (item) {
+          item.created_at = item.created_at.split('T')[0];
+          item.owner = userinfo.owner;
+          item.repo = userinfo.repo;
+        });
+
+        var tempList = that.data.issuesList;
+        that.data.issuesList = tempList.concat(issues);
+        return new Promise(function(resolve, reject){
           resolve();
-        }).catch(function () {
-          reject();
-          return;
-        })
-      });
-    }).then(function () {
-      console.log(that.data.issuesList[0]);
-      // that.data.issuesList.sort(function (a, b) {
-      //   console.log(a);
-      //   return DateTime.parse(a.updated_at) < DateTime.parse(b.updated_at);
-      // });
-      that.setData({
-        issuesList: that.data.issuesList.slice(0, 100)
-      });
-    }).catch(function () {
-      console.log('catch exception')
-      that.count = that.count + 1;
-      that.setData({
-        issuesList: that.data.issuesList.slice(0, 100)
-      });
+        });
+      })
     });
   }
 })
