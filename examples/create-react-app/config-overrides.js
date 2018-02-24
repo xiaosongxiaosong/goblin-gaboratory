d9a0b342a4e31911974bd70eff4b8d172a0701a9
xiaosong <kunkkaco@gmail.com>
2018-02-24 15:24:48 +0800

    feat: create-react-app

---
+++ examples/create-react-app/config-overrides.js
@@ -0,0 +1,11 @@
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
+module.exports = function override(config, env) {
+  // do stuff with the webpack config...
+  config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
+  config = rewireLess.withLoaderOptions({
+    modifyVars: { "@primary-color": "#25A8E1" },
+  })(config, env);
+  return config;
+};
