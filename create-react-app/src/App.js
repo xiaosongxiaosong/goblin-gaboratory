7db446febd68330607b3a0319d5a45c176e8a6a7
xiaosong <kunkkaco@gmail.com>
2018-07-09 19:48:58 +0800

    feat: add antd

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Button from 'antd/lib/button';
 import './App.css';
 
 class App extends Component {
@@ -12,6 +13,9 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p className="App-intro">
+          <Button type="primary">Button</Button>
         </p>
       </div>
     );
